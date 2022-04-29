7672d182f03468383ec7ca67a19cfc2cda3500f1
zhanbowe08 <70243440+zhanbowe08@users.noreply.github.com>
2022-04-29 20:17:31 +1000

    fixed bug for repeating tweets

--- data_harvest/searchTwitter.py
+++ data_harvest/searchTwitter.py
@@ -1,5 +1,5 @@
 import tweepy
-import INFO_2
+import INFO
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import geopandas as gpd
 from shapely.geometry import Point
@@ -11,46 +11,34 @@ class search(tweepy.API):
         super(search, self).__init__()
         self.__userName = 'admin'
         self.__passWord = 'password'
-        # self.__url = 'http://' + self.__userName + ':' + self.__passWord + '@172.26.131.244:5984/'
-        self.__url = 'http://admin:password@localhost:5984/'
+        self.__url = 'http://' + self.__userName + ':' + self.__passWord + '@172.26.131.244:5984/'
         print("Connecting to server...")
+
         self.server = DB.Server(self.__url)
         print("Connected to server")
         self.db = self.server['twitter_new']
+
         self.analyser = SentimentIntensityAnalyzer()
         self.__auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
         self.__auth.set_access_token(access_token, access_secret)
         self.api = tweepy.API(self.__auth, wait_on_rate_limit=True)
-        self.sa2_main16_df = load_sa2_data()
+        #self.sa2_main16_df = load_sa2_data()
         print("Harvester setup complete")
 
     def search_tweet(self):
-        holder = []
         query = 'place:melbourne'
-        start = '201801010000'
-        end = '202204280000'
-        # label = 'SearchAPI'
-        label = 'development'
-
-        status = tweepy.Cursor(self.api.search_full_archive,
-                label = label,
-                query=query,
-                fromDate= start,
-                toDate=end,
-                maxResults=20
-                ).items()
-
-        # status = self.api.search_full_archive(label=label, query=query, fromDate=start, toDate=end, maxResults=100)
+        start = '202203290600'
+        label = '30days'
+        status = tweepy.Cursor(self.api.search_30_day,
+                               label=label,
+                               query=query,
+                               fromDate=start,
+                               maxResults=10
+                               ).pages()
         for each in status:
-            holder.append(each._json)
-        return holder
-
-    def doc(self):
-        while True:
-            print("Next batch of tweets:")
-            tweets = self.search_tweet()
-            for tweet in tweets:
+            for tweet in each:
                 tweet_info = dict()
+                tweet = tweet._json
                 try:
                     if 'extended_tweet' in tweet.keys():
                         text = tweet['extended_tweet']['full_text']
@@ -60,16 +48,22 @@ class search(tweepy.API):
                     print("Tweet KeyError - using default tweet text")
                     text = tweet['text']
                 print(f"Tweet id: {tweet['id']}, date created: {tweet['created_at']}")
+
                 tweet_info['_id'] = str(tweet['id'])
                 tweet_info['type'] = 'search'
                 tweet_info['tweet'] = tweet
                 sentiment = self.analyser.polarity_scores(text)
                 tweet_info['sentiment'] = sentiment
+
                 coords = get_tweet_coordinates(tweet)
+    
+    
                 if coords is not None:
                     tweet_info['sa2'] = get_sa2_main16(coords, self.sa2_main16_df)
+    
+                #print(tweet_info)
                 self.store_tweets(tweet_info)
-            time.sleep(5)
+                time.sleep(1)
 
 
     def store_tweets(self, tweet):
@@ -120,11 +114,11 @@ def get_tweet_coordinates(tweet_doc):
 
 
 if __name__ == '__main__':
-    bear_token = INFO_2.BEAR_TOKEN
-    consumer_key = INFO_2.API_KEY
-    consumer_secret = INFO_2.KEY_SECRET
-    access_token = INFO_2.ACCESS_TOKEN
-    access_secret = INFO_2.TOKEN_SECRET
+    bear_token = INFO.BEAR_TOKEN
+    consumer_key = INFO.API_KEY
+    consumer_secret = INFO.KEY_SECRET
+    access_token = INFO.ACCESS_TOKEN
+    access_secret = INFO.TOKEN_SECRET
 
     t = search(consumer_key, consumer_secret, access_token, access_secret)
     print("Setup complete")
@@ -132,4 +126,4 @@ if __name__ == '__main__':
     coordinates = [144.33363404800002, -38.50298801599996, 145.8784120140001, -37.17509899299995]
 
     print("Searching tweets...")
-    t.doc()
+    t.search_tweet()
