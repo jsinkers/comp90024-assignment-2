52f4684f2001a18cc9cd3f9c2466520e09610a18
James Sinclair <sinclair.j@gmail.com>
2022-04-30 23:49:38 +1000

    bugfix for dockerhub login

--- flask/deploy-flask.sh
+++ flask/deploy-flask.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo "Enter docker hub password for sinkers"
-read -sr PASSWORD_INPUT
-
-. ./openrc.sh; ansible-playbook deploy-flask.yaml -u ubuntu --key-file=~/.ssh/database.pem --extra-vars 'dockerhub_password: $PASSWORD_INPUT'
+. ./openrc.sh; ansible-playbook deploy-flask.yaml -u ubuntu --key-file=~/.ssh/database.pem  --ask-become-pass
