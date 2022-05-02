4dbf9e1470a3b1947257ed6737a0fa6af23798dd
James Sinclair <sinclair.j@gmail.com>
2022-05-02 20:04:28 +1000

    Add Svelte component for Map/MapMarker from Svelte docs

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+	import Map from './Map.svelte';
+	import MapMarker from './MapMarker.svelte';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Map lat={-37.81} lon={144.96} zoom={9}>
+	<!--<MapMarker lat={37.8225} lon={-122.0024} label="Svelte Body Shaping"/>
+	<MapMarker lat={33.8981} lon={-118.4169} label="Svelte Barbershop & Essentials"/>
+	<MapMarker lat={29.7230} lon={-95.4189} label="Svelte Waxing Studio"/>
+	<MapMarker lat={28.3378} lon={-81.3966} label="Svelte 30 Nutritional Consultants"/>
+	<MapMarker lat={40.6483} lon={-74.0237} label="Svelte Brands LLC"/>
+	<MapMarker lat={40.6986} lon={-74.4100} label="Svelte Medical Systems"/>
+	-->
+</Map>
