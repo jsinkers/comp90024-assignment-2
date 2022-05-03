d29eaca0b938652ab233ef577e32b9ba33cc5ca5
lige-on-git <tanlige815@outlook.com>
2022-05-03 20:46:48 +1000

    add a new function to filter out a list of keywords in the same view; add a new function which returns the grouped query results from a view

--- couchback.py
+++ couchback.py
@@ -1,6 +1,7 @@
 import couchdb
 import json
 import requests
+import pandas as pd
 
 
 class CouchInterface:
@@ -33,7 +34,7 @@ class CouchInterface:
         :param view_name: the name of view you want
         :return:
         """
-        resp = requests.get(self.url + database + '/' + design_name + '/_view/' + view_name)
+        resp = requests.get(self.url + database + '/_design/' + design_name + '/_view/' + view_name)
         view = json.loads(resp.text)
         return view
 
@@ -52,7 +53,7 @@ class CouchInterface:
         """
         design_name = '_design/' + design_name
         map_fun = 'function (doc) { var reg = ' + regex + '; ' \
-            'if (reg.test(doc.' + field + ')) { emit(doc.' + field + ', doc); } }'
+            'if (reg.test(doc.' + field + ')) { emit(doc.' + field + ', doc._id); } }'
         data = {'views': {view_name: {'map': map_fun}}}
         requests.put(self.url + database + '/' + design_name, data=json.dumps(data))
         resp = requests.get(self.url + database + '/' + design_name + '/_view/' + view_name)
@@ -75,9 +76,11 @@ class CouchInterface:
         design_name = '_design/' + design_name
         data = {'views': {}}
         for i, view_name in enumerate(views_name):
+            # map_fun = 'function (doc) { var reg = ' + regexes[i] + '; ' \
+            #     'if (reg.test(doc.' + fields[i] + ')) { emit(doc.' + \
+            #           fields[i] + ', doc._id); } }'
             map_fun = 'function (doc) { var reg = ' + regexes[i] + '; ' \
-                'if (reg.test(doc.' + fields[i] + ')) { emit(doc.' + \
-                      fields[i] + ', doc); } }'
+                'if (reg.test(doc.' + fields[i] + ')) { emit(' + '"' + regexes[i][1:-2] + '"' + ', doc._id); } }'
             data['views'][view_name] = {'map': map_fun}
         requests.put(self.url + database + '/' + design_name, data=json.dumps(data))
         views = []
@@ -87,14 +90,71 @@ class CouchInterface:
             views.append(view)
         return views
 
+    def create_regex_view_combined(self, database: str, field: str, regexes: list, design_name: str, view_name: str="election_tweets"):
+        """
+        Similar with create_regex_view, and create_regex_views. However, we search tweets using all regex words in a single view.
+        """
+
+        design_name = '_design/' + design_name
+
+        regs_str = "["
+        for i, reg in enumerate(regexes):
+            regs_str += reg
+            regs_str += ","
+        regs_str = regs_str[:-1]
+        regs_str += "]"
+
+        map_fun = 'function (doc) {' + \
+                      'var regs = ' + regs_str + '; ' + \
+                      'var to_emit = false;' + \
+                        'if (' + field + '.coordinates){' + \
+                            'for (var i in regs) {' + \
+                                'if (regs[i].test(' + field + '.text)){' + \
+                                    'to_emit = true;' + \
+                                '}' + \
+                            '}' + \
+                            'if (to_emit && doc.sa2 && doc.sentiment.compound){' + \
+                                'emit(["election",doc.sa2], doc.sentiment.compound);' + \
+                            '}' + \
+                        '}' + \
+                    '}'
+
+        data = {'views': {view_name: {'map': map_fun}}}
+
+        print(requests.put(self.url + database + '/' + design_name, data=json.dumps(data)).json())
+        resp = requests.get(self.url + database + '/' + design_name + '/_view/' + view_name)
+        view = json.loads(resp.text)
+        return view
+
+    def grouping_results(self, db_name, design_doc, view_name):
+        """
+        a database has a design document and view under the path /db_name/_design/design_doc/_view/view_name
+        """
+        db = self.server[db_name]
+        path = design_doc + '/' + view_name
+        results = []
+        for item in db.view(path, group=True):
+            results.append((item.key, item.value))
+
+        return results
+
 
 if __name__ == '__main__':
-    ci = CouchInterface(address='172.26.131.244', port='5984', username='admin', password='password')
+    # ci = CouchInterface(address='localhost', port='5984', username='admin', password='password')
     # ret = ci.create_regex_view('twitter_new', 'tweet.text', '/melbourne/i', 'test1')
-    ret = ci.create_regex_views('twitter_new',
-                                ['tweet.text', 'tweet.text'],
-                                ['/melbourne/i', '/vote/i'],
-                                'test2',
-                                ['filter_mel', 'filter_vote']
-                                )
+
+    # the keywords to filter out
+    election_keywords = ['/auspol/i', '/ausvotes/i', '/ausvotes2022/i', '/ausvotes22/i',
+    '/ScottMorrison/i', '/Scott/i', '/Morrison/i', '/ScottMorrisonMP/i', '/election/i']
+
+    ci = CouchInterface(address='172.26.131.244', port='5984', username='admin', password='password')
+
+    if("error" in ci.get_view("twitter_historic", "tweets", "election_tweets")): # if view not exist
+        # create a new view
+        ret = ci.create_regex_view_combined('twitter_historic', 'doc.tweet', election_keywords, 'tweets', 'election_tweets')
+
+    # receiving a grouping result from the view
+    results = ci.grouping_results("twitter_historic", "tweets", "election_tweets")
+    print(results)
+
     print('test case ends.')
