e60a28016b9efda2bb0f30f9d7abcfd85a0dcb47
James Sinclair <sinclair.j@gmail.com>
2022-05-04 23:51:05 +1000

    Successful deployment of harvester

--- data_harvest/deployment/deploy-harvester.sh
+++ data_harvest/deployment/deploy-harvester.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-. ./openrc.sh; ansible-playbook deploy-harvester.yaml -u ubuntu --key-file=~/.ssh/database.pem --ask-become-pass #-vvv
+. ./openrc.sh; ansible-playbook deploy-harvester.yaml -u ubuntu --key-file=~/.ssh/database.pem --ask-become-pass -vvv
