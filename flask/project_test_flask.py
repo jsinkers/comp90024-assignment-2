0acba79e2e44e014e492ee5bb88deea5e094d545
lige-on-git <tanlige815@outlook.com>
2022-05-06 14:02:59 +1000

    fix more conflict

--- flask/project_test_flask.py
+++ flask/project_test_flask.py
@@ -2,7 +2,6 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'  # server
 
-<<<<<<< HEAD
 # test_get_URL = "/api/analytics/diversity/sentiment/diversity"
 # response = requests.get(BASE + test_get_URL)
 # print(response.json())
@@ -10,9 +9,4 @@ BASE = 'http://127.0.0.1:5000/'  # server
 
 test_get_URL = "/api/analytics/diversity/language"
 response = requests.get(BASE + test_get_URL)
-=======
-test_get_URL = "/api/analytics/diversity"
-response = requests.get(BASE + test_get_URL)
-
->>>>>>> 2dd6147f46f4cb2546e5c14e22f264573426777f
 print(response.json())
