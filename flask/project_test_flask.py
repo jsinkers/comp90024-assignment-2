6e9225179894fc579bb6acd811c838680d8bee20
lige-on-git <tanlige815@outlook.com>
2022-05-06 23:55:32 +1000

    get two more paths to work

--- flask/project_test_flask.py
+++ flask/project_test_flask.py
@@ -2,11 +2,14 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'  # server
 
-# test_get_URL = "/api/analytics/diversity/sentiment/diversity"
+test_get_URL = "/api/analytics/diversity/sentiment/diversity"
+response = requests.get(BASE + test_get_URL)
+print(response.json())
+
+# test_get_URL = "/api/analytics/diversity/language"
 # response = requests.get(BASE + test_get_URL)
 # print(response.json())
 
-
-test_get_URL = "/api/analytics/diversity/language"
-response = requests.get(BASE + test_get_URL)
-print(response.json())
+# test_get_URL = '/api/analytics/diversity/tweets'
+# response = requests.get(BASE + test_get_URL)
+# print(response.json())
